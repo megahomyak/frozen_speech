a9fc682917df708f5ad75677997c2b7e49f1ff17
megahomyak <g.megahomyak@gmail.com>
2024-11-24 05:57:19 +0500

    Made the participant description generator

--- compile.py
+++ compile.py
@@ -8,6 +8,9 @@ path = os.path.join
 try: shutil.rmtree(compiled_discus_dir := path(compiled_dir := "compiled", "discussions"))
 except: pass
 os.mkdir(compiled_discus_dir)
+try: shutil.rmtree(compiled_partic_dir := path(compiled_dir, "participants"))
+except: pass
+os.mkdir(compiled_partic_dir)
 
 def attrs(text):
     listobj = pairs(text)
@@ -21,7 +24,12 @@ def pairs(text):
     nestlvl = 0
     name = ""
     openindex = 0
+    escaped = False
     for idx, c in enumerate(text):
+        if escaped:
+            name.append(c)
+            escaped = False
+            continue
         if c == "(":
             if nestlvl == 0:
                 openindex = idx
@@ -31,6 +39,8 @@ def pairs(text):
             if nestlvl == 0:
                 items.append((name.strip(), text[openindex+1:idx]))
                 name = ""
+        elif c == "\\":
+            escaped = True
         elif nestlvl == 0:
             name += c
     return items
@@ -47,12 +57,20 @@ def templated(filepath, args):
         nonlocal result
         result += str(arg) + end
     locals_ = {"_": part, "attrs": attrs, "pairs": pairs, **args}
-    exec(read(filepath), {}, locals_)
+    exec(read(filepath), locals_, locals_)
     return result
 
 def write_templated(inpath, outpath, args):
     result = templated(inpath, args)
     write(result, outpath)
+
+def get_participant(filename):
+    return attrs(read("participants", filename))
+
+for participant_fname in os.listdir(participants_dir := "participants"):
+    participant = attrs(read(participants_dir, participant_fname))
+    participant.filename = participant_fname
+    write_templated("participant.html.template", path(compiled_partic_dir, participant_fname), {"participant": participant})
 
 discussions = []
 for discussion_fname in os.listdir(discussions_dir := "discussions"):
@@ -60,10 +78,9 @@ for discussion_fname in os.listdir(discussions_dir := "discussions"):
     participants = set()
     discussion.filename = discussion_fname
     for sender_name, _ in pairs(discussion.messages):
-        assert os.path.exists(path(compiled_dir, "participants", sender_name)), sender_name
         participants.add(sender_name)
     discussion.participants = participants
     discussions.append(discussion)
-    write_templated("discussion.html.template", path(compiled_discus_dir, discussion_fname), {"discussion": discussion})
-discussions.sort(key=lambda discussion: datetime.datetime.strptime(discussion.date, "%d.%m.%Y %H:%M:%S"))
-write_templated("index.html.template", path(compiled_dir, "index.html"), {"discussions": discussions})
+    write_templated("discussion.html.template", path(compiled_discus_dir, discussion_fname), {"discussion": discussion, "get_participant": get_participant})
+discussions.sort(key=lambda discussion: datetime.datetime.strptime(discussion.date, "%d.%m.%Y %H:%M:%S"), reverse=True)
+write_templated("index.html.template", path(compiled_dir, "index.html"), {"discussions": discussions, "get_participant": get_participant})
